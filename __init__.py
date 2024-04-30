e23c55b36467e962f73a4dfac45a7b119d4365fc
smthemex <138738845+smthemex@users.noreply.github.com>
2024-04-30 11:01:46 +0800

    Add files via upload
    init py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+import sys
+
+python = sys.executable
+
+from .ComfyUI_Llama3_8B_Node import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
